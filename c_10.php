6e6db754d1d18f73557b878d6a569fb644e90e60
andy <andy@7shangzuo.com>
2018-09-30 15:28:12 +0800

    性能

---
+++ c_10.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * User: Andy
+ * Date: 2018/9/30
+ * Time: 14:00
+ */
+
+/*
+
+## PEAR
+
+   PHP Extension and Application Repository
+
+
+PEAR 项目的目的就是为了通过提供一个安装和维护的工具
++
+加上代码/发布管理标准来解决这个问题
+
+ 1. PEAR 安装程序 -- 一个代码包管理工具
+  2. 包含PHP代码库的包
+   3. PEAR代码的标准，包括一个版本标准
+
+
+## PECL
+
+    一个从PEAR脱离的项目是PECL，英文全称PHP Extension Community Library.
+PECL 曾经是PEAR的一个子项目，但是今天，它是单独管理的。这意味着PECL有它自己的Web站点、邮件列表、程序管理等等。
+
+    但是，PEAR和PECL共享工具和架构：他们都使用PEAR安装程序，都使用相同的数据包格式，并且都使用相同的版本标准。
+
+    不过编码标准是不一样的：PECL遵循的是PHP编码标准（对于C代码），而PEAR使用它自身的标准
+
+ */
+
+// pear install XML_Parser;
+version_compare($v1, $v2, $operator);
+
+
+// pear list-upgrades commands
+
+/* 获取 PEAR */
+
+// go-pear.org 网站安装PEAR
+
+
+
